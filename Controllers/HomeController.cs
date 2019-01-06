2389754ee12d6490d0efd995d5acd8a0894bd868
chiyanan001 <30824167+chiyanan001@users.noreply.github.com>
2019-01-06 21:55:47 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moniter.Models;
+
+namespace Moniter.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        public IActionResult Indexlis()
+        {
+            return View();
+        }
+
+        public IActionResult IndexBox()
+        {
+            return View();
+        }
+
+
+    }
+
+}
